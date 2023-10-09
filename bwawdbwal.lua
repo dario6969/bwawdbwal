a8e97a6cc25f362dda44ce6d79078a3fdad79214
dario6969 <82063009+dario6969@users.noreply.github.com>
2023-10-10 00:02:36 +0200

    Create bwawdbwal.lua

---
+++ bwawdbwal.lua
@@ -0,0 +1,79 @@
+local ReplicatedStorage = game:GetService('ReplicatedStorage')
+local RunService = game:GetService('RunService')
+
+local MAX_DISTANCE = 28
+
+local remotes = ReplicatedStorage.Remotes
+local player = game.Players.LocalPlayer
+
+local function clearCons()
+    for _, con in shared.Connections do
+        con:Disconnect()
+    end
+
+    table.clear(shared.Connections)
+
+    print('disconnected old')
+end
+
+if shared.Connections then
+    clearCons()
+end
+
+shared.Connections = {}
+
+local function init(character)
+    clearCons()
+
+    local humanoid = character:WaitForChild('Humanoid')
+    local root = character:WaitForChild('HumanoidRootPart')
+
+    humanoid.Died:Once(clearCons)
+
+    local cooldown = false
+
+    local function autoParry()
+        if not root.Parent then
+            return
+        end
+
+        if cooldown then
+            return
+        end
+
+        local pos = root.Position
+
+        local ping = game.Stats.Network.ServerStatsItem['Data Ping']:GetValue() / 1000
+
+        for _, ball in workspace.Balls:GetChildren() do
+            if not (ball:GetAttribute('realBall') and ball:GetAttribute('target') == player.Name) then
+                continue
+            end
+
+            local interpolated = ball.Position + (ball.Velocity * ping * 1.25)
+
+            if (pos - interpolated).Magnitude < MAX_DISTANCE then
+                keypress(0x46)
+
+                cooldown = tick()
+
+                while ball:GetAttribute('target') == player.Name or tick() - cooldown > ping * 2 do
+                    task.wait()
+                end
+
+                cooldown = false
+
+                break
+            end
+        end
+    end
+
+    table.insert(shared.Connections, RunService.PostSimulation:Connect(autoParry))
+    table.insert(shared.Connections, RunService.PreSimulation:Connect(autoParry))
+end
+
+player.CharacterAdded:Connect(init)
+
+if player.Character then
+    init(player.Character)
+end
