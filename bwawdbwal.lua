d17dbba4f92126c6fa8433497be09f67fa476347
dario6969 <82063009+dario6969@users.noreply.github.com>
2023-10-10 05:09:38 +0200

    Update bwawdbwal.lua

--- bwawdbwal.lua
+++ bwawdbwal.lua
@@ -9,6 +9,14 @@ local function clearCons()
 
     table.clear(shared.Connections)
 
+    if shared.Visualizer then
+        shared.Visualizer:Destroy()
+    end
+
+    if shared.Interpolated then
+        shared.Interpolated:Destroy()
+    end
+
     print('disconnected old')
 end
 
@@ -18,6 +26,10 @@ end
 
 shared.Connections = {}
 
+local function isAlive(entity)
+    return entity:FindFirstChild('Humanoid') and entity.Humanoid.Health > 0
+end
+
 local function init(character)
     clearCons()
 
@@ -26,12 +38,84 @@ local function init(character)
 
     humanoid.Died:Once(clearCons)
 
+    local cooldown = false
+
+    local lastTarget
+    local lastClash
+
+    local visualizer = Instance.new('Part')
+    visualizer.Size = Vector3.zero
+    visualizer.Shape = Enum.PartType.Ball
+    visualizer.Color = Color3.fromRGB(255, 255, 255)
+    visualizer.Material = Enum.Material.Neon
+    visualizer.Transparency = 0.9
+    visualizer.CanCollide = false
+    visualizer.Parent = workspace
+
+    local fakeBall = Instance.new('Part')
+    fakeBall.Size = Vector3.one * 3
+    fakeBall.Shape = Enum.PartType.Ball
+    fakeBall.Color = Color3.fromRGB(255, 106, 106)
+    fakeBall.Material = Enum.Material.Neon
+    fakeBall.Transparency = 0.5
+    fakeBall.CanCollide = false
+    fakeBall.Parent = workspace
+
+    shared.Visualizer = visualizer
+    shared.Interpolated = fakeBall
+
+    local function clash(ball)
+        if tick() - (lastClash or 0) > 0.25 then
+            lastClash = 0
+            lastTarget = nil
+
+            return
+        end
+
+        if not (lastTarget and isAlive(lastTarget)) then
+            return
+        end
+
+        local targetRoot = lastTarget:FindFirstChild('HumanoidRootPart')
+
+        if not targetRoot then
+            return
+        end
+
+        local maxDistance = math.clamp(ball.Velocity.Magnitude / 3, 6, 26)
+
+        if (targetRoot.Position - root.Position).Magnitude < maxDistance then
+            lastClash = tick()
+
+            return true
+        end
+    end
+
+    local function setLastTarget(ball)
+        local targetName = ball:GetAttribute('target')
+        local target = targetName and workspace.Alive:FindFirstChild(targetName)
+
+        if not target or target == player.Name or not isAlive(target) then
+            lastTarget = nil
+
+            return
+        end
+
+        lastTarget = target
+    end
+
     local function autoParry()
         if not root.Parent then
             return
         end
 
+        if cooldown then
+            return
+        end
+
         local pos = root.Position
+
+        visualizer.Position = pos
 
         local ping = game.Stats.Network.ServerStatsItem['Data Ping']:GetValue() / 1000
 
@@ -40,11 +124,32 @@ local function init(character)
                 continue
             end
 
-            local interpolated = ball.Position + (ball.Velocity * ping)
-            local distance = 8 + (math.min(ball.Velocity.Magnitude / 1000, 1) * 100) + (ping * 100)
+            local interpolated = ball.Position + (ball.Velocity * (ping * 1.4))
+            local distance = 8 + (math.min(ball.Velocity.Magnitude / 600, 1) * 60)
+
+            if lastTarget and isAlive(lastTarget) and tick() - (lastClash or 0) < ping * 2 and (root.Position - lastTarget.PrimaryPart.Position).Magnitude < 50 then
+                distance *= 1.2
+            end
+
+            fakeBall.Position = interpolated
+            visualizer.Size = Vector3.one * distance
 
             if (pos - interpolated).Magnitude < distance or (pos - ball.Position).Magnitude < distance then
                 keypress(0x46)
+
+                if clash(ball) then
+                    return
+                end
+                
+                cooldown = tick()
+
+                while ball:GetAttribute('target') == player.Name or tick() - cooldown < ping do
+                    task.wait()
+                end
+
+                cooldown = false
+
+                setLastTarget(ball)
 
                 break
             end
